f886aed9af10e92789f5f0e393fee7f6656df1fe
Samuel Jimenez Canizal <110293438+samuelj25@users.noreply.github.com>
2026-06-22 13:45:04 -0400

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.py
@@ -0,0 +1,31 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        """
+        Time Complexity: O(n^2)
+        Space Complexity: O(1)
+        """
+        nums.sort()
+        res = []
+
+        for i, value in enumerate(nums):
+            if (value > 0):
+                break
+
+            if ((i > 0) and (value == nums[i - 1])):
+                continue
+    
+            l, r = i + 1, len(nums) - 1
+            while (l < r):
+                three_sum = value + nums[l] + nums[r]
+
+                if (three_sum == 0):
+                    res.append([value, nums[l], nums[r]])
+                    l += 1
+                    while ((nums[l] == nums[l - 1]) and (l < r)):
+                        l += 1
+                elif (three_sum < 0):
+                    l += 1
+                else:
+                    r -= 1
+        
+        return res
