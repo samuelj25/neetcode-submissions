fd02f31eba5fc2553ebd0e356575feefc767dcde
Samuel Jimenez Canizal <110293438+samuelj25@users.noreply.github.com>
2026-06-29 22:23:30 -0400

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.py
@@ -0,0 +1,25 @@
+class TimeMap:
+
+    def __init__(self):
+        self.key_map = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if (key not in self.key_map):
+            self.key_map[key] = []
+        self.key_map[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        if (key not in self.key_map):
+            return res
+        
+        values = self.key_map[key]
+        l, r = 0, len(values) - 1
+        while (l <= r):
+            m = (l + r) // 2
+            if (values[m][1] <= timestamp):
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+        return res
